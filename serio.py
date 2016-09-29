446a987e91921335a6a843a7679bb4805a3e12f2
Alexey Gadzhiev <alg1973@gmail.com>
2016-09-29 17:35:13 +0300

    now it worked

--- serio.py
+++ serio.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
+import sys
 import termios
+import time
 
 class Serial:
     tty_name=''
@@ -10,6 +13,7 @@ class Serial:
         # Config the debug serial port
         self.oldattrs = termios.tcgetattr(self.tser)
         newattrs = termios.tcgetattr(self.tser)
+	newattrs[2] = newattrs[2] & ~termios.HUPCL
         newattrs[4] = termios.B9600  # ispeed
         newattrs[5] = termios.B9600  # ospeed
         newattrs[3] = newattrs[3] & ~termios.ICANON & ~termios.ECHO
@@ -20,23 +24,33 @@ class Serial:
 
     def sread(self):
         result=""
-        while True:
+        while 1:
             ch=self.tser.read(1)
-            if ch and ch!="\n":
-                if ch != "\r":
+            if ch and ch!="\n" and ch!="\r":
                     result +=ch
             else:
                 break
         return result
 
+    def swrite(self,str):
+	self.tser.write(str)
+
     def sclose(self):
         # Restore previous state
-        termios.tcsetattr(self.tser, termios.TCSAFLUSH, self.oldattrs)
+        #termios.tcsetattr(self.tser, termios.TCSAFLUSH, self.oldattrs)
         self.tser.close()
         # Flush any partial buffer
 
-print "Start\n"
-f=Serial("/dev/tty")
-ch=f.sread()
-print " Yes: "+ch
+if len(sys.argv)==2:
+	tty_name=sys.argv[1] 
+else:
+	tty_name="/dev/cu.wchusbserial1410"
+f=Serial(tty_name)
+time.sleep(3)
+while 1:
+	f.swrite("\n")
+	time.sleep(0.5)
+	ch=f.sread()
+	print ch
+	time.sleep(5)
 f.sclose()

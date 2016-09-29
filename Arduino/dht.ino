522f6eaa072f5b41247cca8951ca955470961f5c
Alexey Gadzhiev <alg1973@gmail.com>
2016-09-29 17:37:04 +0300

    temperature sensor sketch

--- Arduino/dht.ino
+++ Arduino/dht.ino
@@ -1,24 +1,35 @@
 #include "DHT.h"
 
 DHT dht;
+int session_count=0;
 
 void setup()
 {
   Serial.begin(9600);
 
-  dht.setup(2); // data pin 2
+  dht.setup(2,DHT::DHT11); // data pin 2
+  delay(dht.getMinimumSamplingPeriod());
 }
 
 void loop()
 {
-  delay(dht.getMinimumSamplingPeriod());
-  while (Serial.read() != '\n') {  
-          ; 
-  }   
+  /* delay(dht.getMinimumSamplingPeriod()); */
+  int ch=0;
+  do {
+    ch=Serial.read();
+    if (ch=='\r' || ch=='\n') {
+      while (Serial.available()>0) {
+        Serial.read();
+        } 
+        break;
+      }   
+    } while (1); 
   
-
+  Serial.print(dht.getTemperature());
+  Serial.print("\t");
   Serial.print(dht.getHumidity());
   Serial.print("\t");
-  Serial.print(dht.getTemperature());
-  Serial.print("\n");
+  Serial.print(session_count);
+  Serial.print("\r\n");
+  session_count++;
 }

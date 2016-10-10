81081f7d62646190c2e37f620a1e834481f5f57d
Alexey Gadzhiev <alg1973@gmail.com>
2016-10-10 10:26:21 +0300

    Indent terminal friendly and test default editor

--- dyndb.py
+++ dyndb.py
@@ -7,7 +7,8 @@ from boto3.dynamodb.conditions import Key, Attr
 
 class Tempdb:
     def __init__(self,url="http://localhost:8000",region="eu-central-1"):
-    	self.dynamodb = boto3.resource('dynamodb', region_name=region, endpoint_url=url)
+    	self.dynamodb = boto3.resource('dynamodb', region_name=region, 
+					endpoint_url=url)
     	self.table = self.dynamodb.Table('Temperature');
 
     def put_values(self,temperature,humidity,thermo_name="InsideHall"):
@@ -34,7 +35,8 @@ class Tempdb:
     def thermo_get_minutes(self,minutes,thermo_name="InsideHall"):
         response = self.table.query(
             KeyConditionExpression=Key('Thermometer').eq(thermo_name) &
-                                        Key('GetDate').gt(get_val(time.time()-minutes*60))
+                                   Key('GetDate').gt(get_val(time.time()-
+							minutes*60))
             )
         return response
 

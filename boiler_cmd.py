7de81cc2c22fe1ee3c7c52ac14b831ad7c3e17b0
Alexey Gadzhiev <alg1973@gmail.com>
2016-10-13 20:24:26 +0300

    Simple command to write state file

---
+++ boiler_cmd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import tconf
+import bstate
+import sys
+
+
+if len(sys.argv)==3:
+	target=int(sys.argv[1])
+	heating=int(sys.argv[2])
+elif len(sys.argv)==4:
+	target=int(sys.argv[1])
+	heating=int(sys.argv[2])
+        thermo_name=sys.argv[3]
+else:
+	target=100
+	heating=0
+        thermo_name=tconf.thermo_name
+
+
+b=bstate.Boiler(thermo_name)
+print b.write_state(target,heating)
+print b.read_state()

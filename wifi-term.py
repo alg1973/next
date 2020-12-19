9482a1991cdfafb4adc6a5b048d5ff8b13f4ce3c
Alexey Gadzhiev <alg1973@gmail.com>
2020-12-20 02:30:38 +0300

    exceptions

--- wifi-term.py
+++ wifi-term.py
@@ -8,16 +8,20 @@ import sys
 if len(sys.argv)>0:
     os.chdir(os.path.dirname(sys.argv[0]))
 
-r = requests.get("http://192.168.8.250/")
+try:
+    r = requests.get("http://192.168.8.250/")
+except:
+    print ("IO error connecting to term server")
+    sys.exit(1)
 
 if r.status_code != 200:
     print ("the http status is not 200 '{0}'".format(r.status_code))
-    exit
+    sys.exit(1)
 
 lines = r.text.splitlines()
 if len(lines) < 1:
     print ("got empty text")
-    exit
+    sys.exit(1)
 
 while len(lines)>0 and not lines[0]:
     lines.pop(0)

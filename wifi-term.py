f0a990ce03755bf1fbf8af0156807fba00d99805
Alexey Gadzhiev <alg1973@gmail.com>
2021-03-21 18:59:59 +0300

    output option

--- wifi-term.py
+++ wifi-term.py
@@ -9,10 +9,15 @@ if len(sys.argv)>0:
     os.chdir(os.path.dirname(sys.argv[0]))
 
 
-ip = sys.argv[1] if len(sys.argv)>1 and sys.argv[1] else 'http://192.168.8.251/' 
+ip = sys.argv[1] if len(sys.argv)>1 and sys.argv[1] else 'http://192.168.8.250/' 
+suf = sys.argv[2] if len(sys.argv)>2 and sys.argv[2] else '' 
 
 try:
     r = requests.get(ip)
+
+except Exception as e:
+    print("IO error connecting to term server: {}.".format(e))
+    sys.exit(1)
 except:
     print ("IO error connecting to term server")
     sys.exit(1)
@@ -32,10 +37,11 @@ while len(lines)>0 and not lines[0]:
 data = lines[0].split()
 if len(data)<1:
     data.append( -127 )
-with open("./term.log", "a") as wlog:
+logfile = "./term"+suf+".log"
+with open(logfile, "a") as wlog:
     wlog.write("{0} {1}\n".format(int(time.time()), float(data[0])))
-
-with open("./term.last","w") as termstamp:
+stampfile = "./term"+suf+".last"
+with open(stampfile,"w") as termstamp:
     termstamp.write("{0} {1}".format(int(time.time()), float(data[0])))
 
 print ("{0} {1}".format(int(time.time()), float(data[0])))
